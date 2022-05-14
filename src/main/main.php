7ead71b6271395c481500b12092d14c161f24518
matsuyuki <102745877+matsuyuki1231@users.noreply.github.com>
2022-05-14 22:09:17 +0900

    update for pm4

--- src/main/main.php
+++ src/main/main.php
@@ -4,16 +4,17 @@ namespace main;
 
 class main extends \pocketmine\plugin\PluginBase{
 
-	public function onEnable(){
+	public function onEnable():void{
 		$server = \pocketmine\Server::getInstance();
 		$worlddir = "worlds/";
 
 		$count = 0;
 		foreach (scandir($worlddir) as $value) {
 			if(is_dir($worlddir . $value) && ($value !== "." && $value !== "..") ){
-				$server->loadLevel($value) && $count++;
+				$server->getWorldManager()->loadWorld($value,true) && $count++;
 			}
 		}
+
 
 		$this->getLogger()->info("§a$count 個のワールドを読み込みました！");
 	}
